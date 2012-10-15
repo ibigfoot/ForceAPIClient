83c59ac1bd08e56383de295071c857b693cbaf13
Troy Sellers <tsellers@salesforce.com>
2012-10-16 06:55:05 +1100

    more

---
+++ wsClient/src/main/java/com/force/aus/soap/client/EnterpriseClient.java
@@ -0,0 +1,19 @@
+package com.force.aus.soap.client;
+
+public class EnterpriseClient {
+
+	public static void main (String[] args) {
+		EnterpriseClient client = new EnterpriseClient();
+		try {
+			client.run();
+		} catch (Throwable t) {
+			t.printStackTrace();
+		}
+	}
+	
+	public void run() throws Exception {
+		
+		
+		
+	}
+}
