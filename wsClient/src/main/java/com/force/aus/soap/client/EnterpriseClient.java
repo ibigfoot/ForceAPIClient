7882dcf8a07facf89ed5e42fa201f92b7ecf6b50
Troy Sellers <tsellers@salesforce.com>
2012-10-18 07:04:19 +1100

    commit

--- wsClient/src/main/java/com/force/aus/soap/client/EnterpriseClient.java
+++ wsClient/src/main/java/com/force/aus/soap/client/EnterpriseClient.java
@@ -1,11 +1,31 @@
 package com.force.aus.soap.client;
 
+import java.io.InputStream;
+import java.net.URL;
+import java.util.Iterator;
+import java.util.Properties;
+
+import javax.xml.namespace.QName;
+
+import com.force.aus.soap.Login;
+import com.force.aus.soap.LoginResponse;
+import com.force.aus.soap.LoginResultType;
+import com.force.aus.soap.SforceService;
+import com.force.aus.soap.Soap;
+
 public class EnterpriseClient {
 
+	private static Properties props;
+	
 	public static void main (String[] args) {
 		EnterpriseClient client = new EnterpriseClient();
 		try {
+			props = new Properties();
+			InputStream in = client.getClass().getClassLoader().getResourceAsStream(PropertyStrings.PROPERTIES_FILENAME);
+			props.load(in);
+			in.close();
 			client.run();
+
 		} catch (Throwable t) {
 			t.printStackTrace();
 		}
@@ -13,7 +33,31 @@ public class EnterpriseClient {
 	
 	public void run() throws Exception {
 		
+		System.out.println("running");
+		Iterator<Object> it = props.keySet().iterator();
+		while(it.hasNext()) {
+			String key = (String)it.next();
+			System.out.println("Props ["+key+"] -> ["+props.getProperty(key)+"]");
+		}
 		
+		
+		URL url = this.getClass().getResource(props.getProperty(PropertyStrings.WSDL_NAME));
+		if (url == null) {
+			throw new RuntimeException("Unable to find WSDL ["+props.getProperty(PropertyStrings.WSDL_NAME)+"] on classpath");
+		}
+		SforceService service = new SforceService(url, new QName(props.getProperty(PropertyStrings.QNAME_NAMESPACE), 
+													props.getProperty(PropertyStrings.QNAME_LOCAL_PART)));
+		
+		Soap port = service.getSoap();
+		
+		Login login = new Login();
+		login.setPassword(props.getProperty(PropertyStrings.PASSWORD_PROPERTY));
+		login.setUsername(props.getProperty(PropertyStrings.USERNAME_PROPERTY));
+		
+		LoginResponse response = port.login(login);
+		
+		LoginResultType result = response.getResult();
+		System.out.println("Session ID ["+result.getSessionId()+"]");
 		
 	}
 }
---
+++ wsClient/src/main/java/com/force/aus/soap/client/PropertyStrings.java
@@ -0,0 +1,37 @@
+package com.force.aus.soap.client;
+
+/**
+ * Simple interface to hold key values listed in the properties file.
+ * Any new property values added to the file should have keys listed in this interface.
+ * @author tsellers
+ *
+ */
+public interface PropertyStrings {
+
+	/**
+	 * Name of properties file that will be loaded in the main method.
+	 */
+	public static final String PROPERTIES_FILENAME = "connection.properties";
+	/**
+	 * String key for auth endpoint property
+	 */
+	public static final String AUTH_ENDPOINT_PROPERTY = "authendpoint";
+	/**
+	 * String key for username property
+	 */
+	public static final String USERNAME_PROPERTY = "username";
+	/**
+	 * String key for Password property
+	 */
+	public static final String PASSWORD_PROPERTY = "pass";
+
+	/**
+	 * The name of WSDL file used for generation of stub classes
+	 * Should be available on the classpath
+	 */
+	public static final String WSDL_NAME = "wsdl.path";
+	
+	public static final String QNAME_NAMESPACE = "qname.namespace";
+	
+	public static final String QNAME_LOCAL_PART = "qname.local.part";
+}
