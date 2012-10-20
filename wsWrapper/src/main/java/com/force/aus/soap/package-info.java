e17f98c3b6167b0d3651d43e17f42fede8c6d636
Troy Sellers <tsellers@salesforce.com>
2012-10-20 18:05:37 +1100

    removed generated classes

--- wsWrapper/src/main/java/com/force/aus/soap/package-info.java
+++
@@ -1,2 +0,0 @@
-@javax.xml.bind.annotation.XmlSchema(namespace = "urn:sobject.enterprise.soap.sforce.com", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
-package com.force.aus.soap;
